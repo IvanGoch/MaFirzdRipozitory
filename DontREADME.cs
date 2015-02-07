be55bb766fad960d7cc19e544eb2ab8dccd7a268
IvanGoch <Ivan-gochachko@yandex.ru>
2015-02-07 18:45:46 +0300

    Create DontREADME.cs

---
+++ DontREADME.cs
@@ -0,0 +1,23 @@
+# MaFirzdRipozitory
+Description (optional)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a < b)
+            {
+                Console.WriteLine(a);
+                Console.ReadLine();
+            }        
+         }
+    }
+}
